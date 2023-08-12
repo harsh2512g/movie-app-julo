8a4b8dfd15dfcc9e9e00b8a56007d9791e7c3bf6
Harsh Gupta <harsh2512g@gmail.com>
2023-08-12 13:24:33 +0530

    revert app.tsx changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
 // App.tsx
-import React ,{lazy,Suspense} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { MovieProvider } from './context/MovieContext';
-
+import MovieListPage from './pages/MovieListPage';
+import MovieDetailPage from './pages/MovieDetailPage';
+import MyListPage from './pages/MyListPage';
 import GlobalStyles from './GlobalStyles';
 
-const MovieListPage = lazy(() => import('./pages/MovieListPage'));
-const MovieDetailPage = lazy(() => import('./pages/MovieDetailPage'));
-const MyListPage = lazy(() => import('./pages/MyListPage'));
+
 
 const App: React.FC = () => {
   return (
     <Router>
       <GlobalStyles/>
       <MovieProvider>
-        <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-      
           <Route path="/" element={<MovieListPage />} />
           <Route path="/movie/:id" element={<MovieDetailPage />} />
           <Route path="/my-list" element={<MyListPage />} />
         </Routes>
-        </Suspense>
       </MovieProvider>
     </Router>
   );
